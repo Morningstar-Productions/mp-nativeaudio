83db5ff60b0af37a5a9c89e4ee91112dfcd69982
xViperAG <usairsoftingandgaming@gmail.com>
2025-01-13 16:24:49 -0500

    Initial Commit

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'mp-nativeaudio'
+author 'xViperAG'
+version '1.0.0'
+
+files {
+    'data/mp_sounds.dat54.rel',
+	'dlc_rhrpnative/mp_script_audio.awc'
+}
+
+data_file 'AUDIO_WAVEPACK' 'dlc_morningstar'
+data_file 'AUDIO_SOUNDDATA' 'data/mp_sounds.dat'
